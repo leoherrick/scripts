284ac7e86bb1f43126e497cf3b12b8e71aff274b
Leo Herrick <leoherrick@gmail.com>
2012-11-13 19:12:38 -0800

    first commit

---
+++ fresh_html_proj.rb
@@ -0,0 +1,101 @@
+require 'net/http'
+
+## Index Page
+index_text = '<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" title="no title" charset="utf-8">
+    <script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/script.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body id="index">
+    <h1>Sample Page</h1>
+  </body>
+</html>'
+
+## JQuery Text
+jquery_text = ''
+source_file = File.open(ENV["HOME"] + '/code/lib/jquery/jquery-1.8.2.min.js')
+source_file.each do |line|
+  jquery_text += line
+end
+
+## css text 
+css_text = '
+html {
+  background : #4B7399;
+  font-family: Verdana, Helvetica, Arial;
+  font-size: 14px;
+}
+  
+body {
+  width : 980px;
+  margin: 0 auto;
+  background-color: #FFF;
+  padding: 20px 40px;
+  border: solid 1px black;
+  margin-top: 20px;  
+  min-height : 600px;  
+  border : 1px solid black;
+  -moz-box-shadow: 2px 2px 5px #000;
+  -webkit-box-shadow: 2px 2px 5px #000;
+  box-shadow: 2px 2px 5px #000;
+}
+'
+
+## Exceptions
+raise ArgumentError, 'You must add a single command-line argument matching the target project directory' if (ARGV.length == 0)
+raise ArgumentError, 'Too many command-line arguments! This script takes a single argument for the target directory' if (ARGV.length > 1)
+
+## Change project home to target directory
+Dir.chdir(ARGV.first)
+
+## set project_home variable
+proj_dir = Dir.new(Dir.pwd)
+raise StandardError, 'The target directory is not empty. It must be empty before you can run this script' if (proj_dir.entries.length > 2)
+
+File.open("index.html", "w") do |f|
+  f.puts index_text
+end
+
+## create directories
+Dir.mkdir("css")
+Dir.mkdir("js")
+Dir.mkdir("img")
+Dir.mkdir("test")
+Dir.mkdir("test/js")
+
+## create css file
+File.open("css/style.css", "w") do |f|
+  f.puts css_text
+end
+
+## create javascript files
+File.open("js/script.js", "w")
+
+Net::HTTP.start("ajax.googleapis.com") do |http|
+    resp = http.get("/ajax/libs/jquery/1/jquery.min.js")
+    open("js/jquery.min.js", "wb") do |file|
+        file.write(resp.body)
+    end
+end
+
+## create js test file
+File.open("test/js/test.js", "w")
+
+puts 'created all files successfully!
+
+  css/
+    style.css
+  img/
+  index.html
+  js/
+    jquery.min.js
+    script.js
+  test/
+    js/
+      test.js  
+';
+
